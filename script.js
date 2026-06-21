876e2a2f83d14085eecb725fd937ea05c5e0adc7
Priyanshi Yadav <priyanshiy1312@gmail.com>
2026-06-21 11:11:41 +0530

    script.js

--- script.js
+++ script.js
@@ -87,15 +87,7 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
     }
   });
 });
-const hero = document.querySelector(".hero");
-window.addEventListener("mousemove", (e) => {
-  const x = (window.innerWidth / 2 - e.clientX) / 40;
-  const y = (window.innerHeight / 2 - e.clientY) / 40;
 
-  if (hero) {
-    hero.style.transform = `translate(${x}px, ${y}px)`;
-  }
-});
 const glow = document.createElement("div");
 glow.classList.add("cursor-glow");
 document.body.appendChild(glow);
